bd474d0f8f3538a8021a3fb99dc0e38d0825ce82
mikezom <mikezom3721@gmail.com>
2022-06-18 00:17:56 +0800

    smzdm

--- libs/function/usr_cmd/smzdm/smzdm.py
+++ libs/function/usr_cmd/smzdm/smzdm.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from graia.saya import Channel
 from graia.ariadne.app import Ariadne
 from graia.ariadne.model import Group, Member
@@ -8,8 +9,9 @@ from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.ariadne.message.parser.twilight import (
     Twilight,
     FullMatch,
+    RegexMatch,
     RegexResult,
-    WildcardMatch
+    ParamMatch,
 )
 
 from libs.control import Permission
@@ -21,10 +23,13 @@ channel = Channel.current()
 @channel.use(
     ListenerSchema(
         listening_events=[GroupMessage],
-        inline_dispatchers=[Twilight([FullMatch("smzdm"), "anything" @ WildcardMatch()])]
+        inline_dispatchers=[Twilight([
+            FullMatch("smzdm"),
+            "arg" @ ParamMatch(optional = True)
+            ])]
     )
 )
-async def main(app: Ariadne, member: Member, group: Group, anything: RegexResult):
+async def main(app: Ariadne, member: Member, group: Group, arg: RegexResult):
     
     try:
         Permission.group_permission_check(group, "smzdm")
@@ -43,20 +48,14 @@ async def main(app: Ariadne, member: Member, group: Group, anything: RegexResult
             MessageChain(f"不配：{e}")
         )
     
-    if not anything.matched:
+    if not arg.matched:
         await app.send_group_message(
             group,
             SMZDM.parse_smzdm()
         )
-    else:
-        smzdm_num = int(anything.result.display)
-        if (smzdm_num > 5):
-            await app.send_group_message(
-                group,
-                MessageChain(f"不准刷屏！")
-            )
-        else: 
-            await app.send_group_message(
-                group,
-                SMZDM.parse_smzdm(smzdm_num)
-            )
+    elif arg.matched:
+        smzdm_str = arg.result.display
+        await app.send_group_message(
+            group,
+            SMZDM.search_smzdm(smzdm_str)
+        )
--- libs/helper/smzdm.py
+++ libs/helper/smzdm.py
@@ -1,24 +1,128 @@
 import feedparser
 import random
 import time
+import json
+import requests
+from bs4 import BeautifulSoup
 from graia.ariadne.message.chain import MessageChain
+from dateutil import parser
+from thefuzz import process
 
 class SMZDM:
     buffer: dict
-    last_update = time.struct_time
     
-    def need_update() -> bool:
-        return True
+    def get_target_index(l: list) -> int:
+        if len(l) < 30:
+            return len(l)
+        else:
+            return l.index(min(l))
     
-    def parse_smzdm(m: int = 3) -> MessageChain:
+    def time_parser(time: str) -> int:
+        date_time = parser.parse(time)
+        return int(date_time.timestamp())
+    
+    @classmethod
+    def load_buffer(cls) -> None:
+        with open('data/smzdm/smzdm.json') as f:
+            cls.buffer = json.load(f)
+    
+    @classmethod
+    def save_buffer(cls) -> None:
+        with open('data/smzdm/smzdm.json', 'w') as f:
+            f.write(json.dumps(cls.buffer))
+    
+    @classmethod
+    def update_buffer(cls, title, link, time, i):
+        cls.buffer['title'][i] = title
+        cls.buffer['link'][i] = link
+        cls.buffer['time'][i] = cls.time_parser(time)
+    
+    @classmethod
+    def update_smzdm(cls):
+        
+        cls.load_buffer()
+        
         feed = feedparser.parse('http://feed.smzdm.com/')
-
+        
         n = len(feed['entries'])
+        
+        for i in range(n):
+            if feed['entries'][i]['title'] not in cls.buffer['title']:
+                target_index = cls.get_target_index(cls.buffer['time'])
+                cls.update_buffer(
+                    feed['entries'][i]['title'],
+                    feed['entries'][i]['link'],
+                    feed['entries'][i]['published'],
+                    target_index
+                )
+        
+        current_epoch_time = int(time.time())
+        cls.buffer['update_time'] = current_epoch_time
+        
+        cls.save_buffer()
+    
+    @classmethod
+    def check_for_update(cls) -> None:
+        cls.load_buffer()
+        
+        current_epoch_time = int(time.time())
+        if current_epoch_time - cls.buffer['update_time'] > 60 * 60 * 2:
+            cls.update_smzdm()
+    
+    @classmethod
+    def parse_smzdm(cls, m: int = 3) -> MessageChain:
+        cls.check_for_update()
+
+        n = len(cls.buffer['title'])
         random_choices = random.choices(range(n), k = m)
         smzdm_string = ""
 
         for i in random_choices:
-            smzdm_string += feed['entries'][i]['title'] + "\n"
-            smzdm_string += feed['entries'][i]['link'] + "\n"
+            smzdm_string += cls.buffer['title'][i] + "\n"
+            smzdm_string += cls.buffer['link'][i] + "\n"
+        
+        return MessageChain(smzdm_string[:-1])
+    
+    @classmethod
+    def find_smzdm_buffer(cls, s: str) -> MessageChain:
+        cls.check_for_update()
+        
+        fuzz_search_result = process.extract(s, cls.buffer['title'])
+        
+        smzdm_string = "找到以下结果：\n"
+        
+        for title, score in fuzz_search_result:
+            if score >= 50:
+                i = cls.buffer['title'].index(title)
+                smzdm_string += cls.buffer['title'][i] + "\n"
+                smzdm_string += cls.buffer['link'][i] + "\n"
+        
+        if len(smzdm_string) == 8:
+            return MessageChain(f"没找到")
+        else:
+            return MessageChain(smzdm_string[:-1])
+    
+    def search_smzdm(search_str: str) -> MessageChain:
+        smzdm_url = 'https://api.smzdm.com/v1/list?'
+        request_data = {
+            'keyword': search_str,
+            'category_id': '',
+            'brand_id': '',
+            'mall_id': '',
+            'order': 'score',
+            'limit': 5,
+            'offset': '0'
+        }
+
+        res = requests.get(smzdm_url, params = request_data)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        smzdm_data = json.loads(soup.prettify())
+        smzdm_string = ""
+
+        for x in smzdm_data['data']['rows']:
+            if x['article_channel_type'] != 'yuanchuang':
+                smzdm_string += x['article_title']
+                smzdm_string += " " + x['article_price'] + "\n"
+                smzdm_string += x['article_url'] + "\n"
         
         return MessageChain(smzdm_string[:-1])
