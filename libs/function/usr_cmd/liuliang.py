7434677e566b5f003c1fdbb0b61724edbff2f300
mikezom <mikezom3721@gmail.com>
2023-01-10 14:07:59 +0800

    Improved traffic checking module

--- libs/function/usr_cmd/liuliang.py
+++ libs/function/usr_cmd/liuliang.py
@@ -1,8 +1,14 @@
 import requests
-import socket
+# import socket
+import datetime
 import json
+import re
 from lxml import etree
+from loguru import logger
+from bisect import bisect_left
 from graia.saya import Channel
+from graia.scheduler import timers
+from graia.scheduler.saya import SchedulerSchema
 from graia.ariadne.app import Ariadne
 from graia.ariadne.model import Group, Member
 from graia.ariadne.message.chain import MessageChain
@@ -15,9 +21,12 @@ from graia.ariadne.message.parser.twilight import (
 )
 
 from libs.control import Permission
+from libs.helper.info import QQInfoConfig
 
 channel = Channel.current()
 fname = "data/yecao/cookie.txt"
+TRAFFIC_THRESHOLD = [1024, 800, 600, 400, 200, 100, 10, 0]
+previous_traffic = -1
 
 @channel.use(
     ListenerSchema(
@@ -26,7 +35,7 @@ fname = "data/yecao/cookie.txt"
     )
 )
 async def main(app: Ariadne, member: Member, group: Group):
-    
+
     try:
         Permission.group_permission_check(group, "liuliang")
     except Exception as e:
@@ -35,8 +44,8 @@ async def main(app: Ariadne, member: Member, group: Group):
             MessageChain(f"本群不开放此功能，错误信息：{e}")
         )
         raise ExecutionStop()
-    
-    try: 
+
+    try:
         Permission.user_permission_check(member, Permission.DEFAULT)
     except Exception as e :
         await app.send_group_message(
@@ -48,10 +57,12 @@ async def main(app: Ariadne, member: Member, group: Group):
         MessageChain(f"这次真的在查了...")
     )
     try:
-        liuliang_result = liuliang()
+        traffic_info, refresh_date = liuliang()
+        current_time = datetime.datetime.now()
+        time_difference = refresh_date - current_time
         await app.send_group_message(
             group,
-            MessageChain(f"{liuliang_result[0]}\n{liuliang_result[1][:-2]}")
+            MessageChain(f"目前剩余流量: {traffic_info[1]}GB, \n还有 {time_difference.days}天{time_difference.seconds // 3600 % 24}小时{time_difference.seconds // 60 % 60}分{time_difference.seconds % 60}秒 刷新流量")
         )
     except:
         await app.send_group_message(
@@ -59,14 +70,40 @@ async def main(app: Ariadne, member: Member, group: Group):
             MessageChain(f"我靠，又翻车了！")
         )
 
-# def parseCookieFile(cookiefile):
-#     cookies = {}
-#     with open(cookiefile, 'r') as fp:
-#         sp = fp.read().split(';')
-#         for x in sp:
-#             l = x.split('=')
-#             cookies[l[0]] = l[1]
-#     return cookies
+@channel.use(SchedulerSchema(timers.every_hours())) # Check every HOUR
+async def remaining_traffic_warning(app: Ariadne):
+    traffic_info, refresh_date = liuliang()
+    my_group_info = QQInfoConfig.load_file(714870727, 0)
+
+    if previous_traffic > 0 and traffic_info[1] > previous_traffic:
+        # Need Reset
+        my_group_info.traffic_threshold_state = 0
+
+    if my_group_info.traffic_threshold_state >= len(TRAFFIC_THRESHOLD):
+        # Invalid Save
+        logger.error(f"Bad threshold state again, {my_group_info.traffic_threshold_state}")
+        my_group_info.traffic_threshold_state = 0
+
+    if traffic_info[1] < TRAFFIC_THRESHOLD[my_group_info.traffic_threshold_state]:
+        # 超了超了
+        current_time = datetime.datetime.now()
+        time_difference = refresh_date - current_time
+        await app.send_group_message(
+            714870727,
+            MessageChain(f"流量只剩: {traffic_info[1]}GB了！！！, \n还有 {time_difference.days}天{time_difference.seconds // 3600 % 24}小时{time_difference.seconds // 60 % 60}分{time_difference.seconds % 60}秒 刷新流量")
+        )
+        # update traffic_threshold_state
+        rev_thres = list(reversed(TRAFFIC_THRESHOLD))
+        my_group_info.traffic_threshold_state = len(rev_thres) - bisect_left(rev_thres, traffic_info[1])
+
+    logger.info(f"Current threshold for group 714870727 is {TRAFFIC_THRESHOLD[my_group_info.traffic_threshold_state]}")
+    QQInfoConfig.update_file(my_group_info)
+
+    with open('data/yecao/userdata.json', 'r') as f:
+        yecao_config_data = json.load(f)
+    yecao_config_data["previous_traffic"] = traffic_info[1]
+    with open('data/yecao/userdata.json', 'w') as f:
+        f.write(json.dumps(yecao_config_data, indent = 4))
 
 def liuliang():
     # current_ip = socket.getaddrinfo("www.yecao100.org", 80, 0, 0, socket.SOL_TCP)[0][4][0]
@@ -94,6 +131,7 @@ def liuliang():
             'action': 'productdetails',
             'id': yecao_config_data["id"]
         }
+        previous_traffic = yecao_config_data["previous_traffic"]
 
     session.post(login_address, headers=headers, data=payload)
 
@@ -103,6 +141,21 @@ def liuliang():
     r_html = etree.HTML(res.text, etree.HTMLParser())
     r1 = r_html.xpath('//*[@class="panel-body"]')
     r11 = r1[1].xpath('string(.)').strip()
-    result_string = ''.join(r11.split()).split(';')[1].split('使用详情重置流量')
+    r_1 = r11.strip()
+    r_2 = r_1.split('\n')
+    r_3 = [re.sub('\s+', '', x) for x in r_2]
+    traffic_info = [x for x in r_3 if '总共' in x]
+    refresh_date = r_3[20]
+    traffic_info_match_result = re.findall(r'：(\d*)\(GB\)', traffic_info[0])
+    traffic_info_match_result = [int(x) for x in traffic_info_match_result]
+    refresh_data_match_result = re.match(r'(\d\d\d\d-\d\d-\d\d)(\d*):(\d*)', refresh_date)
+    refresh_date_day = refresh_data_match_result.group(1).split('-')
+    refresh_date_day = [int(x) for x in refresh_date_day]
+    refresh_date_hour = int(refresh_data_match_result.group(2))
+    refresh_date_min = int(refresh_data_match_result.group(3))
 
-    return result_string
+    refresh_date_accurate_time = datetime.datetime(*refresh_date_day, refresh_date_hour, refresh_date_min)
+
+    logger.info(f"previous_traffic: {previous_traffic}")
+
+    return traffic_info_match_result, refresh_date_accurate_time
--- libs/helper/info.py
+++ libs/helper/info.py
@@ -11,19 +11,27 @@ from typing import DefaultDict, Set, Tuple, Union
 # from graia.broadcast.builtin.decorators import Depend
 from dataclasses import dataclass
 import json
+from enum import Enum
+
+# TRAFFIC_THRESHOLD = [1024, 800, 600, 400, 200, 100, 10]
+
+class Type_QQ(Enum):
+    GROUP = 0
+    MEMBER = 1
 
 @dataclass
 class QQInfo:
     id: int
     nickname: str = ""
-    
+
     def update_nickname(self, new_nickname: str): self.nickname = new_nickname
-            
+
 
 @dataclass
 class QQGroup(QQInfo):
     repeater_count: int = 0
-    
+    traffic_threshold_state: int = 0
+
     def update_repeater_count(self, new_repeater_count: int): self.repeater_count = new_repeater_count
     def increment_repeater_count(self): self.repeater_count += 1
 
@@ -32,7 +40,7 @@ class QQUser(QQInfo):
     fsm_count: int = 0
 
 class QQInfoConfig:
-    
+
     @classmethod
     def load_file(cls, id, content_type):
         if content_type == 0:
@@ -40,14 +48,16 @@ class QQInfoConfig:
             group_id_string = str(id)
             with open('data/info/group_info.json', 'r') as f:
                 group_info = json.load(f)
-            
+            group_info = cls.refresh_variables_for_groups(group_info)
+
+            with open('data/info/group_info.json', 'w') as f:
+                f.write(json.dumps(group_info, indent = 4))
+
             if group_id_string not in group_info:
                 my_group_info = QQGroup(id)
             else:
-                my_group_info = QQGroup(id,
-                                        group_info[group_id_string]["nickname"],
-                                        group_info[group_id_string]["repeater_count"])
-            
+                my_group_info = QQGroup(*group_info[group_id_string].values())
+
             return my_group_info
 
         elif content_type == 1:
@@ -55,44 +65,53 @@ class QQInfoConfig:
             user_id_string = str(id)
             with open('data/info/user_info.json', 'r') as f:
                 user_info = json.load(f)
-            
+
             if user_id_string not in user_info:
                 my_user_info = QQUser(id)
             else:
                 my_user_info = QQUser(id,
                                       user_info[user_id_string]["nickname"],
                                       user_info[user_id_string]["fsm_count"])
-            
+
             return my_user_info
-    
+
+    @classmethod
+    def refresh_variables_for_groups(cls, group_dict: dict[str, dict]):
+        dummy_group = QQGroup(0)
+        current_qqgroup_variables = dummy_group.__dict__
+        for group_id, group_info in group_dict.items():
+            for variable_name, variable_default_value in current_qqgroup_variables.items():
+                if variable_name not in group_info:
+                    group_dict[group_id][variable_name] = variable_default_value
+        return group_dict
+
     @classmethod
     def update_file(cls, content: Union[QQGroup, QQUser]):
         if type(content) is QQGroup:
             with open('data/info/group_info.json', 'r') as f:
                 group_info = json.load(f)
-                
+
             content_id_string = str(content.id)
             if content_id_string not in group_info:
                 group_info[content_id_string] = {}
-            
-            group_info[content_id_string]["nickname"] = content.nickname
-            group_info[content_id_string]["repeater_count"] = content.repeater_count
-            
+
+            group_info[content_id_string] = content.__dict__
+
             with open('data/info/group_info.json', 'w') as f:
                 f.write(json.dumps(group_info, indent = 4))
-                
-                
+
+
         elif type(content) is QQUser:
             with open('data/info/user_info.json', 'r+') as f:
                 user_info = json.load(f)
-            
+
             user_id_string = str(content.id)
             if user_id_string not in user_info:
                 user_info[user_id_string] = {}
-            
+
             user_info[user_id_string]["nickname"] = content.nickname
             user_info[user_id_string]["fsm_count"] = content.fsm_count
-            
+
             with open('data/info/user_info.json', 'w') as f:
                 f.write(json.dumps(user_info, indent = 4))
         else:
